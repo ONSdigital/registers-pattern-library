bed13c63c5c4e07e3eb604e2463d10faf2db935d
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-16 11:26:13 +0100

    Fix ESLint warnings/errors

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,16 @@
-// Demo component
-// this is only example component
-// you can find tests in __test__ folder
-
 import React from 'react';
 
 class Header extends React.Component {
-    componentDidMount() {
-        // some logic here - we only test if the method is called
-    }
-    render() {
-        return (
-            <div className="my-component">
-              <h1>Header</h1>
-            </div>
-        )
-    }
-};
+  componentDidMount() {
+    // some logic here - we only test if the method is called
+  }
+  render() {
+    return (
+      <div className="my-component">
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
 
 export default Header;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,16 @@
-// Demo component
-// this is only example component
-// you can find tests in __test__ folder
-
 import React from 'react';
 
 class NavBar extends React.Component {
-    componentDidMount() {
-        // some logic here - we only test if the method is called
-    }
-    render() {
-        return (
-            <div className="my-component">
-              <h1>NavBar</h1>
-            </div>
-        )
-    }
-};
+  componentDidMount() {
+    // some logic here - we only test if the method is called
+  }
+  render() {
+    return (
+      <div className="my-component">
+        <h1>NavBar</h1>
+      </div>
+    );
+  }
+}
 
 export default NavBar;
