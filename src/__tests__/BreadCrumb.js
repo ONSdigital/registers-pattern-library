b3b98194c05f5e93335da7f2263103d5f51f1a43
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-18 10:16:54 +0100

    Fix BreadCrumb Enzyme tests
    - Removed other tests due to them clogging up the console, they will be added back in

--- src/__tests__/BreadCrumb.js
+++ src/__tests__/BreadCrumb.js
@@ -10,26 +10,65 @@ import BreadCrumb from '../components/BreadCrumb';
 
 // Shallow Rendering
 // https://github.com/airbnb/enzyme/blob/master/docs/api/shallow.md
-describe('Shallow Rendering', () => {
-  it('to have one `.test-header`s', () => {
-    const wrapper = shallow(<BreadCrumb />);
-    expect(wrapper.find('.test-header')).to.have.length(1);
+describe('BreadCrumb - Shallow Rendering', () => {
+  it('to have 5 `.breadcrumb__item`s', () => {
+    const wrapper = shallow(
+      <BreadCrumb
+        breadCrumbItems={[
+          { name: 'Home', link: '/Home' },
+          { name: 'Enterprise', link: '' },
+          { name: '123456789', link: '/Enterprises/123456789' },
+          { name: 'Legal Unit', link: '' },
+          { name: '987654321', link: '/LegalUnits/987654321' },
+        ]}
+      />,
+    );
+    expect(wrapper.find('.breadcrumb__item')).to.have.length(5);
+  });
+
+  // BreadCrumb will only return a <Link> if the link JSON is empty
+  it('to have 3 links', () => {
+    const wrapper = shallow(
+      <BreadCrumb
+        breadCrumbItems={[
+          { name: 'Home', link: '/Home' },
+          { name: 'Enterprise', link: '' },
+          { name: '123456789', link: '/Enterprises/123456789' },
+          { name: 'Legal Unit', link: '' },
+          { name: '987654321', link: '/LegalUnits/987654321' },
+        ]}
+      />,
+    );
+    expect(wrapper.find('Link')).to.have.length(3);
   });
 });
 
 // Full DOM Rendering
 // https://github.com/airbnb/enzyme/blob/master/docs/api/mount.md
-describe('Full DOM Rendering', () => {
+describe('BreadCrumb - Full DOM Rendering', () => {
   it('allows us to set props', () => {
-    const wrapper = mount(<BreadCrumb bar="baz" />);
-    expect(wrapper.props().bar).to.equal('baz');
+    const items = [
+      { name: 'Home', link: '/Home' },
+      { name: 'Reference Search', link: '' },
+    ];
+    const wrapper = mount(
+      <BreadCrumb breadCrumbItems={items} />,
+    );
+    expect(wrapper.props().breadCrumbItems).to.equal(items);
     wrapper.setProps({ bar: 'foo' });
     expect(wrapper.props().bar).to.equal('foo');
   });
 
   it('calls componentDidMount', () => {
     sinon.spy(BreadCrumb.prototype, 'componentDidMount');
-    const wrapper = mount(<BreadCrumb />);
+    const wrapper = mount(
+      <BreadCrumb
+        breadCrumbItems={[
+          { name: 'Home', link: '/Home' },
+          { name: 'Enterprise', link: '' },
+        ]}
+      />,
+    );
     expect(BreadCrumb.prototype.componentDidMount.calledOnce).to.be.true;
     BreadCrumb.prototype.componentDidMount.restore();
   });
@@ -37,9 +76,17 @@ describe('Full DOM Rendering', () => {
 
 // Static Rendered Markup
 // https://github.com/airbnb/enzyme/blob/master/docs/api/render.md
-describe('Static Rendered Markup', () => {
-  it('renders one `.test-header`s', () => {
-    const wrapper = render(<BreadCrumb />);
-    expect(wrapper.find('.test-header').length).to.equal(1);
+describe('BreadCrumb - Static Rendered Markup', () => {
+  it('renders 3 `.breadcrumb__item`s', () => {
+    const wrapper = render(
+      <BreadCrumb
+        breadCrumbItems={[
+          { name: 'Home', link: '/Home' },
+          { name: 'Enterprise', link: '' },
+          { name: '123456789', link: '/Enterprises/123456789' },
+        ]}
+      />,
+    );
+    expect(wrapper.find('.breadcrumb__item').length).to.equal(3);
   });
 });
