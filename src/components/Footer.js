c88551c546383cb061a908549c3994d3bd12f8fd
ONS-Tom <Tom.Cooling@ons.gov.uk>
2017-10-27 10:41:40 +0100

    Fix/refactor (#8)
    * Add table of contents to README.md
    
    * Fix components to be pure components
    
    - Don't use a react class
    
    * Fix component tests, don't test componentDidMount
    
    - No need to test for componentDidMount as we have migrated to using pure components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,63 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
-class Footer extends React.Component {
-  componentDidMount() {
-    // some logic here - we only test if the method is called
-  }
-  render() {
-    const footerItemStyle = {
-      textDecoration: 'underline',
-      color: 'white',
-    };
-    const footerMailToStyle = {
-      textDecoration: 'underline',
-      color: 'white',
-      cursor: 'pointer',
-    };
-    return (
-      <footer id="footer" className="print--hide" style={{ position: 'absolute', left: 0, right: 0, overflow: 'hidden' }}>
-        <h2 className="visuallyhidden">Footer links</h2>
-        <div className="footer">
-          <div className="wrapper">
-            <nav>
-              <div className="footer-nav col-wrap">
-                {
-                  this.props.footerSection.map((section) => {
-                    return (
-                      <div key={section.title} className="col col--lg-one-third col--md-one-third">
-                        <h3 className="footer-nav__heading">{section.title}</h3>
-                        <ul className="footer-nav__list">
-                          {
-                            section.items.map((item) => {
-                              if (item.emailHref) {
-                                return (
-                                  <li key={item.text} className="footer-nav__item" style={footerMailToStyle} onClick={() => (window.location.href = item.emailHref)}>
-                                    {item.text}
-                                  </li>
-                                );
-                              }
+const Footer = ({ footerSection }) => {
+  const footerItemStyle = {
+    textDecoration: 'underline',
+    color: 'white',
+  };
+  const footerMailToStyle = {
+    textDecoration: 'underline',
+    color: 'white',
+    cursor: 'pointer',
+  };
+  return (
+    <footer id="footer" className="print--hide" style={{ position: 'absolute', left: 0, right: 0, overflow: 'hidden' }}>
+      <h2 className="visuallyhidden">Footer links</h2>
+      <div className="footer">
+        <div className="wrapper">
+          <nav>
+            <div className="footer-nav col-wrap">
+              {
+                footerSection.map((section) => {
+                  return (
+                    <div key={section.title} className="col col--lg-one-third col--md-one-third">
+                      <h3 className="footer-nav__heading">{section.title}</h3>
+                      <ul className="footer-nav__list">
+                        {
+                          section.items.map((item) => {
+                            if (item.emailHref) {
                               return (
-                                <Link key={item.text} to={item.link}>
-                                  <li className="footer-nav__item" style={footerItemStyle}>
-                                    {item.text}
-                                  </li>
-                                </Link>
+                                <li key={item.text} className="footer-nav__item" style={footerMailToStyle} onClick={() => (window.location.href = item.emailHref)}>
+                                  {item.text}
+                                </li>
                               );
-                            })
-                          }
-                        </ul>
-                      </div>);
-                  })
-                }
-              </div>
-            </nav>
-          </div>
+                            }
+                            return (
+                              <Link key={item.text} to={item.link}>
+                                <li className="footer-nav__item" style={footerItemStyle}>
+                                  {item.text}
+                                </li>
+                              </Link>
+                            );
+                          })
+                        }
+                      </ul>
+                    </div>);
+                })
+              }
+            </div>
+          </nav>
         </div>
-      </footer>
-    );
-  }
-}
+      </div>
+    </footer>
+  );
+};
 
 Footer.propTypes = {
   footerSection: PropTypes.array.isRequired,
