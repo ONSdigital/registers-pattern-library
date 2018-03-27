7d6942a32892478ebdb3b09a899b66a2454815c1
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-27 09:45:18 +0100

    Add example test for the Footer

---
+++ src/__tests__/internal/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import Footer from '../../components/internal/Footer';
+
+// Shallow Rendering
+// https://github.com/airbnb/enzyme/blob/master/docs/api/shallow.md
+describe('Footer - Shallow Rendering', () => {
+  it('to have 1 footer class', () => {
+    const wrapper = shallow(
+      <Footer />,
+    );
+    expect(wrapper.find('.page__footer')).to.have.length(1);
+  });
+});
+
+// Static Rendered Markup
+// https://github.com/airbnb/enzyme/blob/master/docs/api/render.md
+describe('Footer - Static Rendered Markup', () => {
+  it('renders the Footer', () => {
+    const wrapper = render(
+      <Footer />,
+    );
+    expect(wrapper.find('.page__footer').length).to.equal(1);
+  });
+});
