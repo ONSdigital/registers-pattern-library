d926465b021a888c6fff32900df9bebf852eec4f
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-27 09:50:33 +0100

    Fix CSS issue by adding images back in

--- src/stories/internalStories.js
+++ src/stories/internalStories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import FooterInternal from '../components/internal/Footer';
-// import './css/internal_ons_css.css';
+import './css/internal_ons_css.css';
 
 storiesOf('Footer (Internal)', module)
   .add('Default Footer with feedbacl', withInfo()(() =>
