431d1ab44f4eaafa5d396f6ced9a992520b33501
urquha <andrew.urquhart@ons.gov.uk>
2018-04-19 15:24:17 +0100

    Ful page example added

--- src/stories/internalStories.js
+++ src/stories/internalStories.js
@@ -11,6 +11,107 @@ import TextInput from '../components/internal/TextInput';
 import TextInputRange from '../components/internal/TextInputRange';
 import './css/internal_ons_css.css';
 import { tradingStatusBands } from '../utils/convertBands';
+import ONSLogo from '../resources/img/logo.svg';
+
+storiesOf('Full Page Examples (Internal)', module)
+  .add('Full Page', withInfo()(() => {
+    // return (
+    //   <div>
+    //     <div style={{ backgroundColor: '#3B7A9E' }}><LinkButton
+    //       id="LinkButton"
+    //       text="This is the link"
+    //       className="username"
+    //     /></div>
+    //     <Button
+    //       id="loginButton"
+    //       className="btn btn--primary venus btn--wide"
+    //       size="wide"
+    //       text="Login"
+    //       onClick={() => alert('Clicked login button...')}
+    //       ariaLabel="Login Button"
+    //       type="submit"
+    //     />
+    //     <Button
+    //       id="loginButton"
+    //       className="btn btn--secondary btn--border"
+    //       size="wide"
+    //       text="Clear"
+    //       onClick={() => alert('Clear')}
+    //       ariaLabel="Clear Button"
+    //       type="submit"
+    //     />
+    //     <Panel
+    //       id="Panel"
+    //       text="Information Here"
+    //       level="info"
+    //       close={() => 1}
+    //       show
+    //     />
+
+
+
+
+
+
+
+
+
+    //     <FooterInternal />
+    //   </div>
+    // );
+    return (
+      <section>
+        <header className="page__header">
+          <div className="logo_container">
+            <div className="wrapper">
+              <div className="group">
+                <div className="col-7">
+                  <div className="logo_header">
+                    <a onClick={() => history.push('/Home')} style={{ cursor: 'pointer' }}>
+                      <img src={ONSLogo} alt="Office for National Statistics" className="logo__img" />
+                    </a>
+                  </div>
+                </div>
+                <div className="col-5">
+                  <div className="header_links">
+                    <ul className="menubar" role="menubar" id="appmenu">
+                      <li className="menubar" role="menuitem">
+                        <LinkButton id="logoutLink" className="username" text="Sign out" onClick={() => alert('Logout')} loading={false} />
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="page_title">
+            <div className="wrapper">
+              <div className="group">
+                <div className="col-12">
+                  <h1 className="jupiter main_heading">Business Index</h1>
+                  <h2 style={{ marginTop: '0px' }} className="neptune sub_heading">A list of UK businesses for statistical use across government</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <section>
+        <div className="main-content">
+          <div className="wrapper">
+            <div className="group">
+              <div className="col-12">
+                <form id="form-sign-in" className="form">
+                  <h3 className="saturn">Page Content</h3>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        </section>
+        <FooterInternal />
+      </section>
+    );
+  }));
 
 storiesOf('Footer (Internal)', module)
   .add('Default Footer with feedback', withInfo()(() =>
@@ -73,7 +174,7 @@ storiesOf('Link Button (Internal)', module)
     />);
   }))
   .add('Link Button spinner', withInfo()(() => {
-    return (<div style={{ backgroundColor: 'blue' }}><LinkButton
+    return (<div style={{ backgroundColor: '#3B7A9E', width: '100px' }}><LinkButton
       id="LinkButton"
       text="This is the link"
       className="username"
@@ -95,7 +196,7 @@ storiesOf('Select Multiple Input (Internal)', module)
   .add('Select Multiple Input', withInfo()(() =>
   (<SelectMultipleInput
     id="SelectMultipleInput"
-    label="Label"
+    label="Trading Status Bands"
     size="42"
     bands={tradingStatusBands}
     onChange={() => alert('Selection Made')}
